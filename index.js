65df1d81e034dd9ac823f1c6c14be17c6e3ec8db
nurdin2004 <adiaderkisohouf@gmail.com>
2025-04-03 09:15:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
+const { expect } = require("chai")
+
 function scuberGreetingForFeet(){
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+describe('scuberGreetingForFeet(feet)',) 
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(){
   // Write your code here!
+ 
+} 
+
+function addFive(someNumber) {
+  
+  function addFive(someNumber) {
+    if (someNumber > 0) {
+      return someNumber + 5;
+    }
+    return someNumber; // If the number is zero or less, return it unchanged.
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  // Write your code here!
+switch (tip) {
+  case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+}
 }
